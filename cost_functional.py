57ded06fe11dca70684717483addf88af64a4fac
Ottar Hellan <37851628+ottarph@users.noreply.github.com>
2021-04-12 01:12:06 +0200

    First test of one step of gradient descent.

--- cost_functional.py
+++ cost_functional.py
@@ -3,13 +3,13 @@ from state_equation import *
 from adjoint_equation import *
 
 
-def cost_functional(Y, W, T, y_d_func, delta_t, V):
+def cost_functional(Y, W, T, y_d_func, delta_t, V, gamma):
     """ For now only the part concerning Y """
     
     cost = 0
     for y, w, t in zip(Y, W, T):
         y_d_func.t = t
         y_d = interpolate(y_d_func, V)
-        cost += delta_t * ( assemble( (y - y_d)*(y - y_d)*dx ) + assemble( w*w*ds ) )
+        cost += 0.5*delta_t * ( assemble( (y - y_d)*(y - y_d)*dx ) + gamma*assemble( w*w*ds ) )
 
     return cost
--- rollout.py
+++ rollout.py
@@ -3,6 +3,8 @@ from state_equation import *
 from adjoint_equation import *
 from cost_functional import *
 
+""" Define domain and space """
+
 T = 3.0                   # final time
 num_steps = 50            # number of time steps
 delta_t = T / num_steps   # time step size
@@ -14,9 +16,9 @@ B = 2
 mesh = RectangleMesh(Point(0, 0), Point(L, B), int(L*10), int(B*10))
 V = FunctionSpace(mesh, "Lagrange", 1)
 
+
 """ Define new boundary-measure to split integral over Gamma_0, Gamma_1 """
 
-#boundary_markers = FacetFunction("size_t", mesh)
 boundary_markers = MeshFunction("size_t", mesh, mesh.topology().dim()-1, 0)
 
 class BoundaryX0(SubDomain):
@@ -38,32 +40,33 @@ bx1.mark(boundary_markers, 1)
 ds = Measure('ds', domain=mesh, subdomain_data=boundary_markers)
 
 
+# Penalty constant for control
+gamma = 1
 
+
+# Physical constants in equation
 rho = 1
 c = 100
 k = 1
 
-tol = 1e-6
 g_const = 10
-# Heat coefficient, function of time and space
-g = Expression("x[1] <=  tol || x[1] > B - tol ? g_const : 0",
-               degree=0, tol=tol, B=B, g_const=g_const, t=0)
-
+# Heat coefficient, function of time
 g = Expression("g_const", degree=0, g_const=g_const, t=0)
 
+# Initial condition
 y_0 = Expression("40 + 30*sin(pi*x[1]/B) + 10*cos(2*pi*x[0]/L)", degree=2, B=B, L=L)
 
 
-# Control, function of time
+# Control over boundary, assumed function of time
 W = []
 w = Expression("t < 1.5 ? 0 : 100", degree=1, t=0)
 for k in range(num_steps):
     w.t = k * delta_t
     w_k = interpolate(w, V)
-    print(assemble(w_k * ds))
     W.append(w_k)
 
 
+# Target temperature function over time
 y_d_const = 10
 y_d_func = Expression("y_d_const + 10*t", degree=0, y_d_const=y_d_const, t=0)
 
@@ -84,7 +87,8 @@ for (y_n, t_n) in zip(Y, T):
 
     stateFile << (y, t_n)
 
-print(cost_functional(Y, W, T, y_d_func, delta_t, V))
+
+print(cost_functional(Y, W, T, y_d_func, delta_t, V, gamma))
 
 
 
@@ -97,7 +101,7 @@ y_d = interpolate(y_d_func, V)
 
 P = adjoint(V, Y, T, y_d_func, g, rho, c, k, delta_t, num_steps, ds)
 
-# Create PVD file for saving solution
+# Create PVD file for saving adjoint
 adjFile = File('rollout/adjoint_equation.pvd')
 
 # Save adjoint equation solution to file
@@ -110,3 +114,42 @@ for p_n, t_n in zip(P, T):
     adjFile << (p, t_n)
 
 
+""" ----------------- Compute gradient ----------------- """
+
+grad = []
+for k, (t, p, w) in enumerate(zip(T, P, W)):
+    g.t = t
+    gg = interpolate(g, V)
+    gk = Function(V)
+    gk.vector()[:] = gg.vector()[:] * p.vector()[:] + gamma * w.vector()[:]
+    #gk.assign(interpolate(g * p + gamma * w, V))
+    grad.append(gk)
+
+# Create PVD file for saving gradient
+gradFile = File('rollout/gradient.pvd')
+
+# Save adjoint equation solution to file
+gk = Function(V)
+for g_k, t_k in zip(grad, T):
+
+    # Save to file
+    gk.assign(g_k)
+
+    gradFile << (gk, t_k)
+
+
+""" Compute solution for incremented control """
+
+step_length = 1
+
+W_new = []
+for w, g in zip(W, grad):
+    w_new = Function(V)
+    w_new.vector()[:] = w.vector()[:] - step_length*g.vector()[:]
+    W_new.append(w_new)
+
+Y_new, _ = state(W_new, V, y_0, g, rho, c, k, delta_t, num_steps, ds)
+
+""" Compute cost functional for new control """
+
+print(cost_functional(Y_new, W_new, T, y_d_func, delta_t, V, gamma))
