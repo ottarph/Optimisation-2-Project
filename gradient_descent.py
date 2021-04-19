More change of y_d from expression to function. Fixed sign in armijo descent condition in gradient descent. Fixed bug caused by heat variable g being changed by gradient loop variable also named g in gradient descent. Changes in test problems.

gradient_descent.py
@@ -6,7 +6,7 @@ from tools import *
 import numpy as np
 
 
-def gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta_t, num_steps, ds,
+def gradient_descent(W_0, V, y_0, y_d, w_a, w_b, gamma, g, rho, c, k, delta_t, num_steps, ds,
          stop, max_iter, rel_stop=0.95, c1 = 0.5, tau=0.5, max_inner_iter=10):
 
     costs = []
@@ -15,7 +15,7 @@ def gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta
     Y, T = state(W, V, y_0, g, rho, c, k, delta_t, num_steps, ds)
 
     last_cost = np.inf
-    curr_cost = cost_functional(Y, W, T, y_d_func, delta_t, V, gamma)
+    curr_cost = cost_functional(Y, W, T, y_d, delta_t, V, gamma)
 
     i = 0
     while curr_cost > stop and i < max_iter and curr_cost / last_cost < rel_stop:
@@ -23,11 +23,12 @@ def gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta
         costs.append(curr_cost)
 
         i += 1
+        print()
         print(f'step number {i}')
         print(f'cost = {curr_cost}')
-        print()
 
-        P = adjoint_eq(V, Y, T, y_d_func, g, rho, c, k, delta_t, num_steps, ds)
+
+        P = adjoint_eq(V, Y, T, y_d, g, rho, c, k, delta_t, num_steps, ds)
 
         grad = []
         for n, (t, p, w) in enumerate(zip(T, P, W)):
@@ -38,7 +39,6 @@ def gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta
             grad.append(gn)
 
         grad_square_norm = 0
-
         for gn in grad:
             grad_square_norm += delta_t * norm(gn)**2
 
@@ -51,21 +51,22 @@ def gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta
             j += 1
 
             W_new = []
-            for w, g in zip(W, grad):
+            for w, gn in zip(W, grad):
                 w_new = Function(V)
-                w_new.vector()[:] = w.vector()[:] - alpha*g.vector()[:]
+                w_new.vector()[:] = w.vector()[:] - alpha*gn.vector()[:]
                 W_new.append(w_new)
 
             Y_new, _ = state(W_new, V, y_0, g, rho, c, k, delta_t, num_steps, ds)
 
-            new_cost = cost_functional(Y_new, W_new, T, y_d_func, delta_t, V, gamma)
+            new_cost = cost_functional(Y_new, W_new, T, y_d, delta_t, V, gamma)
 
-            if new_cost <= curr_cost + c1 * alpha * grad_square_norm:
+            if new_cost <= curr_cost - c1 * alpha * grad_square_norm:
                 accept = True
             else:
                 alpha *= tau
 
         print(f'j = {j}')
+        
 
         """ Project onto admissible set """
 
@@ -78,7 +79,7 @@ def gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta
         """ Compute solution and cost for new admissible control """
 
         Y_new_ad, _ = state(W_new_ad, V, y_0, g, rho, c, k, delta_t, num_steps, ds)
-        new_cost_ad = cost_functional(Y_new_ad, W_new_ad, T, y_d_func, delta_t, V, gamma)
+        new_cost_ad = cost_functional(Y_new_ad, W_new_ad, T, y_d, delta_t, V, gamma)
 
         """ Update loop variables """
 
@@ -111,28 +112,34 @@ def main():
 
     """ Define new boundary-measure to split integral over Gamma_0, Gamma_1 """
     ds = create_boundary_measure(mesh, L, B)
-    
-    """ Defining the cost functional """
 
-    # Penalty constant for control
-    gamma = 1
-
-    # Target temperature function over time
-    y_d_const = 30
-    y_d_func = Expression("y_d_const + 5*t", degree=0, y_d_const=y_d_const, t=0)
-
-
+    """ Properties of system """
     # Physical constants in equation
     rho = 1
     c = 100
-    k = 1
+    k = 50
 
-    g_const = 10
+    g_const = 15
     # Heat coefficient, function of time
-    g = Expression("g_const", degree=0, g_const=g_const, t=0)
+    g = Expression("g_const * exp(-pow(t - T/2, 2))", degree=2, g_const=g_const, T=T, t=0)
 
     # Initial condition
-    y_0 = Expression("40 + 30*sin(pi*x[1]/B) + 10*cos(2*pi*x[0]/L)", degree=2, B=B, L=L)
+    y_0 = Expression("200 + 50*exp(-pow(x[0]-L/2, 2) - pow(x[1]-B/2, 2))", degree=2, L=L, B=B)
+    
+
+    """ Defining the cost functional """
+
+    # Penalty constant for control
+    gamma = 0.5
+
+    # Target temperature at end time
+    y_d_const = 30
+    y_d_func = Expression("y_d_const + 5*t", degree=0, y_d_const=y_d_const, t=T)
+    y_d = project(y_d_func, V)
+
+    w_target_func = Expression("40", degree=1, t=0)
+
+    y_d = create_test_problem(w_target_func, V, y_0, g, rho, c, k, delta_t, num_steps, ds)
 
 
     """ Define admissible set """
@@ -143,7 +150,7 @@ def main():
 
     # Control over boundary, assumed function of time
     W_0 = []
-    w = Expression("t < 1.5 ? 4 : 90", degree=1, t=0)
+    w = Expression("10 + 9 * t", degree=1, t=0)
     for i in range(num_steps):
         w.t = i * delta_t
         w_i = interpolate(w, V)
@@ -151,9 +158,9 @@ def main():
 
     stop = 0 # Don't stop because of low cost functional.
     max_iter = 10
-    max_inner_iter = 10 # How many iterations to find a suitable step length
+    max_inner_iter = 20 # How many iterations to find a suitable step length
 
-    W, costs = gradient_descent(W_0, V, y_0, y_d_func, w_a, w_b, gamma, g, rho, c, k, delta_t, num_steps, ds,
+    W, costs = gradient_descent(W_0, V, y_0, y_d, w_a, w_b, gamma, g, rho, c, k, delta_t, num_steps, ds,
          stop, max_iter, rel_stop=0.95, c1 = 0.5, tau=0.5, max_inner_iter=max_inner_iter)
 
     fname = "temp.txt"